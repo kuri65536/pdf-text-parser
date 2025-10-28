12f1a438a2c485b0901898ac312f2928c733aef2
shimoda <kuri65536@hotmail.com>
2025-10-29 08:03:47 +0900

    feat: changes the method to get text to `get_selected_text`

--- src/pdf_text_parser/pdf_get_text.nim
+++ src/pdf_text_parser/pdf_get_text.nim
@@ -9,14 +9,34 @@ License: MIT, see LICENSE
 import pdf_page
 
 
-proc pdf_get_text*(page: PdfPage, x, y, w, h: float): string =
-    ## .. note:: todo ... implement
+proc pdf_get_text0*(page: PdfPage, x, y, w, h: float): string =
+    ##[ gets the text by `poppler_page_get_text_for_area` .
+    ]##
+    var tmp: cstring
+    let p_page = page.page
+    let (x2, y2) = (x + w, y + h)
+    {.emit: """ {
+                PopplerRectangle rect = {.x1 = `x`, .y1 = `y`,
+                                         .x2 = `x2`, .y2 = `y2`};
+                `tmp` = poppler_page_get_text_for_area(`p_page`, &rect);
+                }
+            """.}
+    if isNil(tmp):
+        return ""
+    return $tmp
+
+
+proc pdf_get_text*(page: PdfPage, x1, y1, x2, y2: float): string =
+    ##[ gets the text by `poppler_page_get_selected_text`
+        with the type of `POPPLER_SELECTION_GLYPH` .
+    ]##
     var tmp: cstring
     let p_page = page.page
     {.emit: """ {
-                PopplerRectangle rect = {.x1 = `x`, .y1 = `y`,
-                                         .x2 = `x` + `w`, .y2 = `y` + `h`};
-                `tmp` = poppler_page_get_text_for_area(`p_page`, &rect);
+                PopplerRectangle rect = {.x1 = `x1`, .y1 = `y2`,
+                                         .x2 = `x2`, .y2 = `y2`};
+                `tmp` = poppler_page_get_selected_text(
+                        `p_page`, POPPLER_SELECTION_GLYPH, &rect);
                 }
             """.}
     if isNil(tmp):
