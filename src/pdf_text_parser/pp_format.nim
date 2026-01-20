babaa0dabf61803aa6ab2a427a82610fe83a65d8
shimoda <kuri65536@hotmail.com>
2026-01-20 22:33:27 +0900

    feat: format the float text to the integer text

--- src/pdf_text_parser/pp_format.nim
+++ src/pdf_text_parser/pp_format.nim
@@ -6,6 +6,24 @@ import logging
 import strutils
 
 
+proc format_float*(fmt, src: string): string =
+    ##[ format a string as the float text and convert it to the integer text
+    ]##
+    let src = src.replace(",", "")
+    let fmt = fmt[0 ..^ 2]
+    let n = try:   parseInt(fmt)
+            except ValueError:
+                error("format:float: can't parse the format ... " & fmt)
+                0
+    let val = try:   int(parseFloat(src) + 0.4)
+              except ValueError:
+                error("format:float: can't parse the value ... " & src)
+                0
+    if n < 1:
+        return $val
+    return strutils.align($val, n)
+
+
 proc format*(fmt, src: string): string =
     ##[ format a string to the specific format `fmt`
 
@@ -14,6 +32,8 @@ proc format*(fmt, src: string): string =
 
         .. note:: todo ... specify the number type at format
     ]##
+    if fmt.endsWith("f"):
+        return format_float(fmt, src)
     let src = src.replace(",", "")
     debug("output:csv:enter... ", src, ", ", fmt)
     if len(fmt) < 1:
